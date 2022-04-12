7d747e133e19426024ff879e4bfe408630415bca
quan200699 <maplestory2012vn@gmail.com>
2022-04-12 14:00:42 +0700

    first commit

---
+++ src/store.js
@@ -0,0 +1,26 @@
+import thunk from "redux-thunk";
+import {applyMiddleware, compose, createStore} from "redux";
+import rootReducer from "./reducer/index";
+
+const initialState = {};
+const middleware = [thunk];
+
+let store;
+
+if (window.navigator.userAgent.includes('Chrome')) {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware),
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    )
+} else {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(applyMiddleware(...middleware))
+    )
+}
